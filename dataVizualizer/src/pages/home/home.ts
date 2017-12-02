28646013562e9ae90ef65112fcebd1255de71083
Marco Maigua <marco.maigua1346@gmail.com>
2017-12-02 12:17:32 -0600

    initiating generic graph

--- dataVizualizer/src/pages/home/home.ts
+++ dataVizualizer/src/pages/home/home.ts
@@ -42,19 +42,19 @@ export class HomePage {
   private svg: any;
   private g: any;
 
-//MusicGenreVariables
-private titleMusicGenre:"Music Genre";
-private widthMusicGenre:number;
-private heightMusicGenre: number;
-private marginMusicGenre={top: 20, right: 20, bottom: 30, left: 40};
-private xMusicGenre:any;
-private yMusicGenre:any;
-private svgMusicGenre:any;
-private gMusicGenre:any;
+  //MusicGenreVariables
+  private titleMusicGenre: "Music Genre";
+  private widthMusicGenre: number;
+  private heightMusicGenre: number;
+  private marginMusicGenre = { top: 20, right: 20, bottom: 30, left: 40 };
+  private xMusicGenre: any;
+  private yMusicGenre: any;
+  private svgMusicGenre: any;
+  private gMusicGenre: any;
 
-//Cities Variables
-//Genre Variables
-//Profesion Variables
+  //Cities Variables
+  //Genre Variables
+  //Profesion Variables
 
   constructor(public navCtrl: NavController,
     public menuCtrl: MenuController,
@@ -147,78 +147,103 @@ private gMusicGenre:any;
   //INITIALIZE MUSIC GENRE BAR GRAPH
   initMusicGenre() {
 
-//     //INIT SVG
-//     this.svgMusicGenre=d3.select("#musicGenre")
-//     .append("svgMusicGenre")
-//     .attr("width","100%")
-//     .attr("heigth","100%")
-//     .attr('viewBox','0 0 900 500');
-//     this.gMusicGenre=this.svgMusicGenre.append("gMusicGenre")
-//     .attr("transform","translate(" + this.marginMusicGenre.left + "," + this.marginMusicGenre.top + ")");
-    
-//     //INIT AXIS
-//     this.yMusicGenre=d3Scale.scaleBand().rangeRound([0,this.heightMusicGenre]).padding(0.1);
-//    this.xMusicGenre=d3Scale.scaleLinear().rangeRound([this.widthMusicGenre]); 
-//   this.yMusicGenre.domain(StatsBarChart.map((d)=>d.musicGenre));
-//   this.xMusicGenre.domain([0,d3Array.max(StatsBarChart,(d)=>d.percentage)]);
+    //     //INIT SVG
+    //     this.svgMusicGenre=d3.select("#musicGenre")
+    //     .append("svgMusicGenre")
+    //     .attr("width","100%")
+    //     .attr("heigth","100%")
+    //     .attr('viewBox','0 0 900 500');
+    //     this.gMusicGenre=this.svgMusicGenre.append("gMusicGenre")
+    //     .attr("transform","translate(" + this.marginMusicGenre.left + "," + this.marginMusicGenre.top + ")");
 
-//    //DRAW AXIS
-//    this.g.append("gMusicGenre")
-//    .attr("class","axis axis--x")
-//   .attr("transform","translate(0," + this.widthMusicGenre + ")")
-// .call(d3Axis.axisLeft(this.xMusicGenre));
-// this.g.append("gMusicGenre")
-// .attr("class","axis axis--y")
-// .attr(d3Axis.axisBottom()
-//     //DRAW BARS
+    //     //INIT AXIS
+    //     this.yMusicGenre=d3Scale.scaleBand().rangeRound([0,this.heightMusicGenre]).padding(0.1);
+    //    this.xMusicGenre=d3Scale.scaleLinear().rangeRound([this.widthMusicGenre]); 
+    //   this.yMusicGenre.domain(StatsBarChart.map((d)=>d.musicGenre));
+    //   this.xMusicGenre.domain([0,d3Array.max(StatsBarChart,(d)=>d.percentage)]);
+
+    //    //DRAW AXIS
+    //    this.g.append("gMusicGenre")
+    //    .attr("class","axis axis--x")
+    //   .attr("transform","translate(0," + this.widthMusicGenre + ")")
+    // .call(d3Axis.axisLeft(this.xMusicGenre));
+    // this.g.append("gMusicGenre")
+    // .attr("class","axis axis--y")
+    // .attr(d3Axis.axisBottom()
+    //     //DRAW BARS
   }
-  initGenre(){
-     //INIT SVG
-     this.svgMusicGenre = d3.select("#musicGenre")
-     .append("svgMusicGenre")
-     .attr("width", '100%')
-     .attr("height", '100%')
-     .attr('viewBox', '0 0 900 500');
-   /*
-   this.svg = d3.select("svg");
-   this.width = +this.svg.attr("width") - this.margin.left - this.margin.right;
-   this.height = +this.svg.attr("height") - this.margin.top - this.margin.bottom;
-   */
-   this.g = this.svgMusicGenre.append("gMusicGenre")
-     .attr("transform", "translate(" + this.marginMusicGenre.left + "," + this.marginMusicGenre.top + ")");
+  initGenre() {
+    //    //INIT SVG
+    //    this.svgMusicGenre = d3.select("#musicGenre")
+    //    .append("svgMusicGenre")
+    //    .attr("width", '100%')
+    //    .attr("height", '100%')
+    //    .attr('viewBox', '0 0 900 500');
+    //  /*
+    //  this.svg = d3.select("svg");
+    //  this.width = +this.svg.attr("width") - this.margin.left - this.margin.right;
+    //  this.height = +this.svg.attr("height") - this.margin.top - this.margin.bottom;
+    //  */
+    //  this.g = this.svgMusicGenre.append("gMusicGenre")
+    //    .attr("transform", "translate(" + this.marginMusicGenre.left + "," + this.marginMusicGenre.top + ")");
 
-   //INIT AXIS
-   this.xMusicGenre = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
-   this.yMusicGenre = d3Scale.scaleLinear().rangeRound([this.height, 0]);
-   this.xMusicGenre.domain(StatsBarChart.map((d) => d.musicGenre));
-   this.yMusicGenre.domain([0, d3Array.max(StatsBarChart, (d) => d.percentage)]);
+    //  //INIT AXIS
+    //  this.xMusicGenre = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
+    //  this.yMusicGenre = d3Scale.scaleLinear().rangeRound([this.height, 0]);
+    //  this.xMusicGenre.domain(StatsBarChart.map((d) => d.musicGenre));
+    //  this.yMusicGenre.domain([0, d3Array.max(StatsBarChart, (d) => d.percentage)]);
 
-   //DRAW AXIS
-   this.g.append("g")
-     .attr("class", "axis axis--x")
-     .attr("transform", "translate(0," + this.height + ")")
-     .call(d3Axis.axisBottom(this.x));
-   this.g.append("g")
-     .attr("class", "axis axis--y")
-     .call(d3Axis.axisLeft(this.y).ticks(10, "%"))
-     .append("text")
-     .attr("class", "axis-title")
-     .attr("transform", "rotate(-90)")
-     .attr("y", 6)
-     .attr("dy", "0.71em")
-     .attr("text-anchor", "end")
-     .text("MusicData");
+    //  //DRAW AXIS
+    //  this.g.append("g")
+    //    .attr("class", "axis axis--x")
+    //    .attr("transform", "translate(0," + this.height + ")")
+    //    .call(d3Axis.axisBottom(this.x));
+    //  this.g.append("g")
+    //    .attr("class", "axis axis--y")
+    //    .call(d3Axis.axisLeft(this.y).ticks(10, "%"))
+    //    .append("text")
+    //    .attr("class", "axis-title")
+    //    .attr("transform", "rotate(-90)")
+    //    .attr("y", 6)
+    //    .attr("dy", "0.71em")
+    //    .attr("text-anchor", "end")
+    //    .text("MusicData");
 
-   //DRAW BARS
-   this.g.selectAll(".bar")
-     .data(StatsBarChart)
-     .enter().append("rect")
-     .attr("class", "bar")
-     .attr("x", (d) => this.x(d.musicGenre))
-     .attr("y", (d) => this.y(d.percentage))
-     .attr("width", this.x.bandwidth())
-     .attr("height", (d) => this.height - this.y(d.percentage));
+    //  //DRAW BARS
+    //  this.g.selectAll(".bar")
+    //    .data(StatsBarChart)
+    //    .enter().append("rect")
+    //    .attr("class", "bar")
+    //    .attr("x", (d) => this.x(d.musicGenre))
+    //    .attr("y", (d) => this.y(d.percentage))
+    //    .attr("width", this.x.bandwidth())
+    //    .attr("height", (d) => this.height - this.y(d.percentage));
   }
-  initProfession(){}
+
+  //GENERIC
+  initProfession(htmlContainer: string,
+    htmlSvg: string,
+    svgWidth: string,
+    svgHeight: string,
+    gTitle: string,
+    marginLeft: string,
+    marginTop: string) {
+    //INIT SVG
+    this.svg = d3.select(htmlContainer)
+      .append(htmlSvg)
+      .attr("width", "100%")
+      .attr("heigth", "100%")
+      .attr("viewBox", "0 0 " + svgWidth + " " + svgHeight);
+    this.g = this.svg.append(gTitle)
+      .attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
+
+    //INIT AXIS
+    this.x = d3Scale.scaleBand().rangeRound([0, +svgWidth]).padding(0.1);
+    this.y = d3Scale.scaleLinear().rangeRound([+svgHeight, 0]);
+    this.x.domain(StatsBarChart.map((d) => d.musicGenre));
+    this.y.domain([0, d3Array.max(StatsBarChart, (d) => d.percentage)]);
+
+
+  }
 
 }
